444d0579b015fe5080109b3255663ae4c885f516
sonumonutonu <43175322+sonumonutonu@users.noreply.github.com>
2019-06-29 12:58:19 +0530

    Update TrackerTest.java

--- core/src/test/java/training/taylor/timetracker/core/TrackerTest.java
+++ core/src/test/java/training/taylor/timetracker/core/TrackerTest.java
@@ -2,7 +2,7 @@ package training.taylor.timetracker.core;
 
 import static org.junit.Assert.*;
 
-import org.junit.Test;
+//import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
