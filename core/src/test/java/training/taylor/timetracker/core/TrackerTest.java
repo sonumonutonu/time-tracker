e88fa614012c2a6bc535aff7d2b4e63d94034e2d
sonumonutonu <43175322+sonumonutonu@users.noreply.github.com>
2019-06-29 13:00:45 +0530

    Update TrackerTest.java

--- core/src/test/java/training/taylor/timetracker/core/TrackerTest.java
+++ core/src/test/java/training/taylor/timetracker/core/TrackerTest.java
@@ -2,7 +2,7 @@ package training.taylor.timetracker.core;
 
 import static org.junit.Assert.*;
 
-//import org.junit.Test;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
@@ -21,7 +21,7 @@ public class TrackerTest {
     @Test
     public void testMe() {
         assertNotNull(tracker);
-    
+    }
 
     @Test
     public void testAdd() {
