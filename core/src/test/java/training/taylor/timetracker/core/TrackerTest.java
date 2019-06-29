9168d76764f25fb005c96709ef4dd445dd014fff
sonumonutonu <43175322+sonumonutonu@users.noreply.github.com>
2019-06-29 16:36:06 +0530

    Update TrackerTest.java

--- core/src/test/java/training/taylor/timetracker/core/TrackerTest.java
+++ core/src/test/java/training/taylor/timetracker/core/TrackerTest.java
@@ -2,12 +2,12 @@
 
 //import static org.junit.Assert.*;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import training.taylor.timetracker.core.dao.TimeEntry;
+// import org.junit.Test;
+// import org.junit.runner.RunWith;
+// import org.springframework.beans.factory.annotation.Autowired;
+// import org.springframework.test.context.ContextConfiguration;
+// import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+// import training.taylor.timetracker.core.dao.TimeEntry;
 
 /**
  * Created by Jason on 6/19/2015.
